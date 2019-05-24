1fac3dcbd479593e9a3bf38326d7ceaf5f779189
xxxxxthhh <xxxxxthhh@gmail.com>
2019-05-24 14:50:19 +0800

    finish the coding of TW Round2-2

--- main/main.js
+++ main/main.js
@@ -1,4 +1,36 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(num_str) {
+    var levelOne = ['._.','...','._.','._.','...','._.','._.','._.','._.','._.'];
+    var levelTwo = ['|.|','..|','._|', '._|','|_|','|_.','|_.','..|','|_|','|_|'];
+    var levelThree = ['|_|','..|','|_.','._|','..|','._|','|_|','..|','|_|','..|'];
+
+    var len_s = num_str.length;
+    var lcd = '';
+    
+    for (i=0;i<len_s;i++){
+        lcd = lcd + levelOne[num_str[i]];
+        if (i == (len_s-1)){
+            lcd = lcd + '\n';
+        }else {
+            lcd = lcd + ' ';
+        }
+    }
+
+    for (i=0;i<len_s;i++){
+        lcd = lcd + levelTwo[num_str[i]];
+        if (i == (len_s-1)){
+            lcd = lcd + '\n';
+        }else {
+            lcd = lcd + ' ';
+        }
+    }
+
+    for (i=0;i<len_s;i++){
+        lcd = lcd + levelThree[num_str[i]];
+        if (i == (len_s-1)){
+            lcd = lcd + '\n'; // are you kidding me??
+        }else {
+            lcd = lcd + ' ';
+        }
+    }
+    return lcd
 };
