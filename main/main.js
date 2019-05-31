f2810b1b3581cb788bb25b918d27c4a8c1d07259
xxxxxthhh <xutianhao@xutianhaodeMacBook-Air.local>
2019-05-31 21:05:35 +0800

    refactor the ans

--- main/main.js
+++ main/main.js
@@ -1,36 +1,24 @@
+const num_list = [
+    ['._.','...','._.','._.','...','._.','._.','._.','._.','._.'],
+    ['|.|','..|','._|', '._|','|_|','|_.','|_.','..|','|_|','|_|'],
+    ['|_|','..|','|_.','._|','..|','._|','|_|','..|','|_|','..|'],
+]
+
 module.exports = function main(num_str) {
-    var levelOne = ['._.','...','._.','._.','...','._.','._.','._.','._.','._.'];
-    var levelTwo = ['|.|','..|','._|', '._|','|_|','|_.','|_.','..|','|_|','|_|'];
-    var levelThree = ['|_|','..|','|_.','._|','..|','._|','|_|','..|','|_|','..|'];
 
     var len_s = num_str.length;
     var lcd = '';
-    
-    for (i=0;i<len_s;i++){
-        lcd = lcd + levelOne[num_str[i]];
-        if (i == (len_s-1)){
-            lcd = lcd + '\n';
-        }else {
-            lcd = lcd + ' ';
-        }
-    }
 
-    for (i=0;i<len_s;i++){
-        lcd = lcd + levelTwo[num_str[i]];
-        if (i == (len_s-1)){
-            lcd = lcd + '\n';
-        }else {
-            lcd = lcd + ' ';
-        }
-    }
+    var level1 = '';
+    var level2 = '';
+    var level3 = '';
 
-    for (i=0;i<len_s;i++){
-        lcd = lcd + levelThree[num_str[i]];
-        if (i == (len_s-1)){
-            lcd = lcd + '\n'; // are you kidding me??
-        }else {
-            lcd = lcd + ' ';
-        }
+    for (i=0; i<len_s; i++){
+        level1+=num_list[0][num_str[i]]+ ' ';
+        level2+=num_list[1][num_str[i]]+ ' ';
+        level3+=num_list[2][num_str[i]]+ ' ';
     }
+    lcd = level1.slice(0,-1) + '\n' +  level2.slice(0,-1) + '\n' + level3.slice(0,-1) + '\n';
+
     return lcd
 };
